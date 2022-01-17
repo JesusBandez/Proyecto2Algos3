4980b30fc7acbe3cfd2aff9ccdc7d592651baaec
rcgn01 <57400723+rcgn01@users.noreply.github.com>
2022-01-16 20:00:11 -0400

    añadidos comentarios

--- ve/usb/grafoLib/ApareamientoPerfectoAvido.kt
+++ ve/usb/grafoLib/ApareamientoPerfectoAvido.kt
@@ -1,37 +1,56 @@
 
-// algoritmo 2
+// Algoritmo 2
 package ve.usb.grafoLib
 import java.util.PriorityQueue
 
-
+// Implementacion de la heuristica avida para obtener el apareamiento perfecto de un grafo G
+// Se recibe de entrada un grafo no dirigido con numero par de vertices 
+// De salida el algoritmo devuelve una lista m, con un conjunto de lados que representan el apareamiento perfecto de G
 class ApareamientoPerfectoAvido(val g:GrafoNoDirigido){
 
+
+    //Variables para almacenar datos usados por el algoritmo
     var m : MutableList<Arista> = mutableListOf()
     var v2 : MutableList<Int> = mutableListOf()
     val l: PriorityQueue<Arista>
 
     init{
 
+        //Se verifica que el grafo g tenga un numero par de vertices
+        //Se lanza una excepcion si el numero de vertices es impar
+        if( g.obtenerNumeroDeVertices() % 2 != 0){
+            throw RuntimeException("El numero de vertices del grafo no es par")
+        }
+
+        // Se añade a la lista v2 todos los vertices de v
         for (vertice in 0..g.obtenerNumeroDeVertices()-1){
             v2.add(vertice)
         }
 
+        // Se crea una cola de prioridad para almacenar los lados del grafo
+        // Los lados se ordenan de forma ascendente segun su peso
+        // El tope de la cola de prioridad es siempre el lado con menor peso
         val compareByCost: Comparator<Arista> = compareBy { it.peso }
-
         l = PriorityQueue<Arista>(compareByCost)
 
+        // Se añaden las aristas del grafo a la cola de prioridad
         for (arista in g.aristas()){
-            // cola de prioridad con min heap, como la cabeza es el min está ordenada de manera creciente
+            
             l.add(arista)
         }
 
+        
         while (v2.size!=0){
 
+            // Se remueve el tope de la cola de prioridad y se asigna a la variable arista
             var arista : Arista = l.remove()
 
+
+            // Se asignan a las variables i y j cada vertice de la arista
             var i : Int = arista.cualquieraDeLosVertices()
             var j : Int = arista.elOtroVertice(arista.cualquieraDeLosVertices())
 
+            // Se verifica si los vertices de la arista se encuentran en la lista v2
             if (v2.contains( i ) && v2.contains( j )){
 
                 //añadir el lado a M
@@ -43,7 +62,7 @@ class ApareamientoPerfectoAvido(val g:GrafoNoDirigido){
             }
         }
     }
-
+    // Funcion para retornar la lista con el apareamiento de G
     fun  obtenerApareamiento() : Iterable<Arista>{
         return m
     }
--- ve/usb/grafoLib/ApareamientoVertexScan.kt
+++ ve/usb/grafoLib/ApareamientoVertexScan.kt
@@ -1,22 +1,35 @@
 
-// algoritmo 3
+// Algoritmo 3
 package ve.usb.grafoLib
 
+// Implementacion de la heuristica Vertex-Scan para obtener el apareamiento perfecto de un grafo G
+// Se recibe de entrada un grafo no dirigido con numero par de vertices 
+// De salida el algoritmo devuelve una lista m, con un conjunto de lados que representan el apareamiento perfecto de G
 class ApareamientoVertexScan(val g:GrafoNoDirigido){
 
+    
+
+    //Variables para almacenar datos usados por el algoritmo
     var v2 : MutableList<Int> = mutableListOf()
     var e2 : MutableList<Arista> = mutableListOf()
     val m : MutableList<Arista> = mutableListOf()
 
-    var contador : Int = 0
+    
 
     init{
 
+        //Se verifica que el grafo g tenga un numero par de vertices
+        //Se lanza una excepcion si el numero de vertices es impar
+        if( g.obtenerNumeroDeVertices() % 2 != 0){
+            throw RuntimeException("El numero de vertices del grafo no es par")
+        }
 
+        // Se añade a la lista v2 todos los vertices de v
         for (vertice in 0..g.obtenerNumeroDeVertices()-1){
             v2.add(vertice)
         }
 
+        // Se añaden todas las arista de g a la variable e2
         e2 = g.aristas() as MutableList
 
         // Se ordenan las aristas por costo, esto significa que si tengo el vertice i y quiero encontrar la arista (i,j)
@@ -31,6 +44,7 @@ class ApareamientoVertexScan(val g:GrafoNoDirigido){
             var i : Int = v2.random()
             lateinit var aristaMin : Arista
 
+            // Se busca en e2 la primera arista en la que incida i
             for (arista in e2){
                 if (arista.cualquieraDeLosVertices() == i 
                     || arista.elOtroVertice(arista.cualquieraDeLosVertices()) == i){
@@ -39,10 +53,13 @@ class ApareamientoVertexScan(val g:GrafoNoDirigido){
                 }
             }
 
+            // Se agrega a la variable j el valor del otro vertice de la arista
             var j = aristaMin.elOtroVertice(i)
 
+            // Se añade a la lista m la arista encontrada
             m.add(aristaMin)
 
+            // Se remueven de v2 los vertices i y j
             v2.remove(i)
             v2.remove(j)
             
@@ -52,7 +69,8 @@ class ApareamientoVertexScan(val g:GrafoNoDirigido){
                    
         }
     }
-    
+
+    // Funcion para retornar la lista con el apareamiento de G   
     fun  obtenerApareamiento() : Iterable<Arista>{
         return m
     }
